6876786a5009960e919b8b556f485d7becc73d88
jlouissaint13 <114112722+jlouissaint13@users.noreply.github.com>
2025-04-03 21:51:52 -0400

    Starting on database and controller logic for registration

--- src/main/java/com/ksumobileapp/Registration/RegisterService.java
+++ src/main/java/com/ksumobileapp/Registration/RegisterService.java
@@ -10,7 +10,7 @@ public class RegisterService {
 public void databaseConnection(RegisterModel registerModel) throws SQLException {
 
     String url = "jdbc:sqlite:accounts.db";
-    String sql = "INSERT INTO users(firstName,lastName,email,password,phone,dob,classification,major) VALUES(?,?,?,?,?,?,?,?)";
+    String sql = "INSERT INTO users(firstName,lastName,personalEmail,campusEmail,password,phone,dob,classification,major) VALUES(?,?,?,?,?,?,?,?,?)";
 
     try (Connection connection = DriverManager.getConnection(url)) {
 
@@ -19,11 +19,12 @@ public void databaseConnection(RegisterModel registerModel) throws SQLException 
             pstmt.setString(1,registerModel.getFname());
             pstmt.setString(2,registerModel.getLname());
             pstmt.setString(3,registerModel.getEmail());
-            pstmt.setString(4,registerModel.getPassword());
-            pstmt.setString(5,registerModel.getPhone());
-            pstmt.setString(6,registerModel.getDob());
-            pstmt.setString(7,registerModel.getClassification());
-            pstmt.setString(8,registerModel.getMajor());
+            pstmt.setString(4,generateEmail(registerModel));
+            pstmt.setString(5,registerModel.getPassword());
+            pstmt.setString(6,registerModel.getPhone());
+            pstmt.setString(7,registerModel.getDob());
+            pstmt.setString(8,registerModel.getClassification());
+            pstmt.setString(9,registerModel.getMajor());
             pstmt.executeUpdate();
 
         }
@@ -38,7 +39,27 @@ public void databaseConnection(RegisterModel registerModel) throws SQLException 
 public void registerUser(RegisterModel registerModel) {
    System.out.println(registerModel.getFname());
 }
-public void generateEmail() {
+public String generateEmail(RegisterModel registerModel) {
+
+
+
+    StringBuilder sb = new StringBuilder();
+    //get the first letter of the students first name
+    sb.append(registerModel.getFname().charAt(0));
+
+    //if last name of student is equal to 5 or greater then take six letters and add to email;
+    //otherwise just add 3 letters
+    if (registerModel.getLname().length() >= 6) {
+        sb.append(registerModel.getLname().substring(0,6));
+        sb.append("@students.kennesaw.edu");
+        System.out.println(sb);
+        String campusEmail = new String(sb);
+        return campusEmail.toLowerCase();
+    }
+    sb.append(registerModel.getLname().substring(0,3));
+    sb.append("@students.kennesaw.edu");
+    String campusEmail = new String(sb);
+    return campusEmail.toLowerCase();
 
 }
 }
